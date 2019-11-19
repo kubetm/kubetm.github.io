e21f629ec828686e78c67d1293890cb7b3c018d2
kubetm <kube.taemin@gmail.com>
2019-11-19 14:56:48 +0900

    4

--- js/main.js
+++ js/main.js
@@ -1,95 +1,42 @@
-/*
-	Future Imperfect by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+// Flyout Menu Functions
+var flyoutmenus = {
+  ".lang-toggle": "#lang-menu",
+  ".share-toggle": "#share-menu",
+  ".nav-toggle": "#site-nav-menu"
+};
 
-(function($) {
+$.each(flyoutmenus, function( key, value ) {
+  $(key).on("click", function() {
+    $(".flyout-menu").not($(value + ".flyout-menu")).removeClass("active");
+    if ($("#wrapper").hasClass('overlay') && $(value).hasClass('active')) {
+      $("#wrapper").removeClass('overlay');
+    } else {
+      $("#wrapper").addClass('overlay');
+    }
+    $(value).toggleClass('active');
+  });
+});
 
-	var	$window = $(window),
-		$body = $('body'),
-		$menu = $('#menu'),
-		$sidebar = $('#sidebar'),
-		$main = $('#main');
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ]
-		});
+// Click anywhere outside a flyout to close
+$(document).on("click", function(e) {
+  if ($(e.target).is(".lang-toggle, .lang-toggle span, #lang-menu, .share-toggle, .share-toggle i, #share-menu, .search-toggle, search.toggle i, #search-menu, .nav-toggle, .nav-toggle i, #site-nav") === false) {
+    $(".flyout-menu").removeClass("active");
+    $("#wrapper").removeClass('overlay');
+  }
+});
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+// Check to see if the window is top if not then display button
+$(window).scroll(function() {
+  if ($(this).scrollTop()) {
+    $('#back-to-top').fadeIn();
+  } else {
+    $('#back-to-top').fadeOut();
+  }
+});
 
-	// Menu.
-		$menu
-			.appendTo($body)
-			.panel({
-				delay: 500,
-				hideOnClick: true,
-				hideOnSwipe: true,
-				resetScroll: true,
-				resetForms: true,
-				side: 'right',
-				target: $body,
-				visibleClass: 'is-menu-visible'
-			});
-
-	// Search (header).
-		var $search = $('#search'),
-			$search_input = $search.find('input');
-
-		$body
-			.on('click', '[href="#search"]', function(event) {
-
-				event.preventDefault();
-
-				// Not visible?
-					if (!$search.hasClass('visible')) {
-
-						// Reset form.
-							$search[0].reset();
-
-						// Show.
-							$search.addClass('visible');
-
-						// Focus input.
-							$search_input.focus();
-
-					}
-
-			});
-
-		$search_input
-			.on('keydown', function(event) {
-
-				if (event.keyCode == 27)
-					$search_input.blur();
-
-			})
-			.on('blur', function() {
-				window.setTimeout(function() {
-					$search.removeClass('visible');
-				}, 100);
-			});
-
-	// Intro.
-		var $intro = $('#intro');
-
-		// Move to main on <=large, back to sidebar on >large.
-			breakpoints.on('<=large', function() {
-				$intro.prependTo($main);
-			});
-
-			breakpoints.on('>large', function() {
-				$intro.prependTo($sidebar);
-			});
-
-})(jQuery);
+// Click event to scroll to top
+$('#back-to-top').click(function() {
+  $('html, body').animate({scrollTop: 0}, 1000);
+  return false;
+});
